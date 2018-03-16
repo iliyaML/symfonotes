83b4b2000cd3f04a4bb4330fcb65b8aba19e718b
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-03-15 22:45:59 -0500

    Pass hardcode data to view

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -6,13 +6,15 @@
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-    class ArticleController extends Controller {
+    class NoteController extends Controller {
         /**
          * @Route("/")
          * @Method({"GET"})
          */
         public function index(){
+            $notes = ['Note 1', 'Note 2'];
+
             // return new Response ('<html><body>Hello</body></html>');
-            return $this->render('articles/index.html.twig');
+            return $this->render('notes/index.html.twig', array('notes' => $notes));
         }
     }
