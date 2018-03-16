a04d0f72da2f924c888fc1bcbb8023af0f5579a5
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-03-15 23:52:45 -0500

    Display data from db

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -1,6 +1,8 @@
 <?php
     namespace App\Controller;
 
+    use App\Entity\Note;
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -12,9 +14,38 @@
          * @Method({"GET"})
          */
         public function index(){
-            $notes = ['Note 1', 'Note 2'];
+            // $notes = ['Note 1', 'Note 2'];
+            $notes = $this->getDoctrine()->getRepository(Note::class)->findAll();
 
             // return new Response ('<html><body>Hello</body></html>');
             return $this->render('notes/index.html.twig', array('notes' => $notes));
         }
+
+        /**
+         * @Route("/note/{id}")
+         */
+        public function show($id){
+            $note = $this->getDoctrine()->getRepository(Note::class)->find($id);
+
+            return $this->render('notes/show.html.twig', array('note' => $note));
+        }
+
+        // /**
+        //  * @Route("/note/save")
+        //  */
+        // public function save(){
+        //     $entityManager = $this->getDoctrine()->getManager();
+
+        //     $note = new Note();
+        //     $note->setTitle('Note Two');
+        //     $note->setBody('This is the body for note two');
+
+        //     // Tells us to eventually save it
+        //     $entityManager->persist($note);
+
+        //     // Actually save it
+        //     $entityManager->flush();
+
+        //     return new Response('Saved a note with the id of ' . $note->getId());
+        // }
     }
--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -25,4 +25,25 @@ class Note
      * @ORM\Column(type="text")
      */
     private $body;
+
+    // Getters & Setters
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function setTitle($title){
+        $this->title = $title;
+    }
+
+    public function getBody(){
+        return $this->body;
+    }
+
+    public function setBody($body){
+        $this->body = $body;
+    }
 }
